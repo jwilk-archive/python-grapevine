9dbd108092028b31677427636194e380a586b8b7
Jakub Wilk <jwilk@jwilk.net>
2017-03-31 18:43:41 +0200

    setup: add Python version check.

--- setup.py
+++ setup.py
@@ -31,6 +31,8 @@ import os
 import distutils.core
 from distutils.command.sdist import sdist as distutils_sdist
 
+int(*['0'], base=0)  # Python >= 2.6 is required
+
 try:
     import sphinx.setup_command as sphinx_setup_command
 except ImportError:
